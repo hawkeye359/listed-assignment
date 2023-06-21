d8afa8d8cbb6b2bcf3a9b5fec1a52046b4aaa70b
huzaifa <mohdhuzaifa359@gmail.com>
2023-06-21 20:29:14 +0530

    project setup

---
+++ src/pages/components/NextFont.ts
@@ -0,0 +1,8 @@
+import { Montserrat, Lato } from 'next/font/google';
+const roboto = Montserrat({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-Montserrat',
+  preload: true,
+});
