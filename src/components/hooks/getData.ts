716ea0d8faea5229b16ef55d9519a378ac3ed912
huzaifa <mohdhuzaifa359@gmail.com>
2023-06-26 18:27:36 +0530

    corrected the fetch data url

--- src/components/hooks/getData.ts
+++ src/components/hooks/getData.ts
@@ -8,7 +8,7 @@ export default function useData() {
   });
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch('http://localhost:3000/api/data');
+      const res = await fetch('/api/data');
       const { revenue, transactions, likes, users } = await res.json();
       console.log(revenue, transactions, likes, users);
       setData({ revenue, transactions, likes, users });
